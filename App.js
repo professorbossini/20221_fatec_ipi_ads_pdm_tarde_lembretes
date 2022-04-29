2ecb5302c83f8f83664f4714bf541303e018ccdc
Rodrigo Bossini <professorbossini@gmail.com>
2022-04-29 19:08:55 -0300

    Finalizamos a apostila 05: Lembretes com apenas um componente

--- App.js
+++ App.js
@@ -1,20 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { 
+  Button,
+  FlatList,
+  ScrollView,
+  StyleSheet, 
+  Text,
+  TextInput, 
+  View 
+} from 'react-native';
+
 
 export default function App() {
+  const [lembrete, setLembrete] = useState('')
+  const[lembretes, setLembretes] = useState([])
+  const[contadorLembretes, setContadorLembretes] = useState(0)
+
+  const capturarLembrete = (lembreteDigitado) => {
+    setLembrete(lembreteDigitado)
+  }
+
+  const adicionarLembrete = () => {
+    //operador spread
+    //const lista = [1, 2, 3]
+    //...lista
+    setLembretes(lembretes => {
+      setContadorLembretes(contadorLembretes + 1)
+      return [{key: contadorLembretes.toString(), value: lembrete}, ...lembretes]
+    })
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.telaPrincipalView}>
+      <View>
+        {/* usuário irá inserir lembretes aqui */}
+        <TextInput
+          onChangeText={capturarLembrete} 
+          placeholder='Lembrar...'
+          style={{borderBottomColor: 'black', borderBottomWidth: 1, marginBottom: 8, padding: 12}}
+        />
+        <Button
+          title="Adicionar lembrete"
+          onPress={adicionarLembrete}
+        />
+      </View>
+      <FlatList 
+        data={lembretes}
+        renderItem={
+          l => (
+            <View
+              style={styles.itemNaLista}>
+                <Text>{l.item.value}</Text>
+            </View>
+          )
+        }
+      />
+       
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  telaPrincipalView: {
+    padding: 40   
   },
+  itemNaLista: {
+    padding: 12,
+    backgroundColor: '#CCC',
+    borderColor: '#000',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginTop: 8
+  }
 });
+{/* <ScrollView>
+        
+        {
+          lembretes.map((l) => (
+            <View 
+              key={l}
+              style={styles.itemNaLista}>
+                <Text>{l}</Text>
+            </View>
+          ))
+        }
+      </ScrollView>  */}
